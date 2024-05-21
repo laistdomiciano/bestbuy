18dcb17aa647a8a53fe059dfee3ac6d7f5a23ad9
Lais  Trevizan Domiciano <laistdomiciano@gamil.com>
2024-05-21 19:19:15 +0100

    create Product class

---
+++ main.py
@@ -0,0 +1,18 @@
+from products import Product
+def main():
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    bose.show()
+    mac.show()
+
+    bose.set_quantity(1000)
+    bose.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ products.py
@@ -0,0 +1,44 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        if not name or price < 0 or quantity < 0:
+            raise ValueError("Invalid input: name cannot be empty, price and quantity must be non-negative.")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        if quantity < 0:
+            raise ValueError("Quantity cannot be negative.")
+        self.quantity = quantity
+        if self.quantity == 0:
+            self.deactivate()
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+    def show(self):
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+    def buy(self, quantity):
+        if not self.active:
+            raise Exception("Product is not active.")
+        if quantity <= 0:
+            raise ValueError("Quantity to buy should be greater than zero.")
+        if quantity > self.quantity:
+            raise Exception("Not enough quantity in stock.")
+
+        total_price = self.price * quantity
+        self.quantity -= quantity
+        if self.quantity == 0:
+            self.deactivate()
+
+        return total_price
