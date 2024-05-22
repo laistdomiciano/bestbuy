df099ac5c74625da35cd722063d71d0e8a9bd9a3
Lais  Trevizan Domiciano <laistdomiciano@gamil.com>
2024-05-22 15:12:36 +0100

    Adding Store Class

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from products import Product
+from store import Store
+
+
 def main():
-    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-    mac = Product("MacBook Air M2", price=1450, quantity=100)
+    product_list = [
+        Product("MacBook Air M2", price=1450, quantity=100),
+        Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+        Product("Google Pixel 7", price=500, quantity=250),
+    ]
 
-    print(bose.buy(50))
-    print(mac.buy(100))
-    print(mac.is_active())
+    store = Store(product_list)
+    products = store.get_all_products()
+    print(f"Total quantity in store: {store.get_total_quantity()}")
 
-    bose.show()
-    mac.show()
-
-    bose.set_quantity(1000)
-    bose.show()
+    order_cost = store.order([(products[0], 1), (products[1], 2)])
+    print(f"Order cost: {order_cost} dollars.")
 
 
 if __name__ == "__main__":
--- store.py
+++ store.py
@@ -1,3 +1,26 @@
+from typing import List, Tuple
+from products import Product
 
+class Store:
+    def __init__(self, products: List[Product]):
+        self.products = products
 
+    def add_product(self, product: Product):
+        self.products.append(product)
 
+    def remove_product(self, product: Product):
+        self.products.remove(product)
+
+    def get_total_quantity(self) -> int:
+        total_quantity = sum(product.get_quantity() for product in self.products)
+        return total_quantity
+
+    def get_all_products(self) -> List[Product]:
+        return [product for product in self.products if product.is_active()]
+
+    def order(self, shopping_list: List[Tuple[Product, int]]) -> float:
+        total_price = 0
+        for product, quantity in shopping_list:
+            if product.is_active():
+                total_price += product.buy(quantity)
+        return total_price
