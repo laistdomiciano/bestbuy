1bd1ecce8ec8ad8a59d2156349de396494846088
Lais  Trevizan Domiciano <laistdomiciano@gamil.com>
2024-05-23 13:27:15 +0100

    Other menu functions

--- main.py
+++ main.py
@@ -11,6 +11,29 @@ product_list = [
 best_buy = Store(product_list)
 
 
+def list_products():
+    products = best_buy.get_all_products()
+    for product in products:
+        print(product.show())
+
+def show_total_amount():
+    print(f"Total quantity in store: {best_buy.get_total_quantity()}")
+
+def make_order():
+    print("When you want to finish order, enter empty text.")
+    user_choice2 = int(input("Which product # do you want? "))
+    products = best_buy.get_all_products()
+    try:
+        order_cost = best_buy.order([(products[0], 1), (products[1], 2)])
+        print(f"Order cost: {order_cost} dollars.")
+    except Exception as e:
+        print(f"Order failed: {e}")
+
+def quit():
+    print("Exiting the store menu.")
+    exit()
+
+
 def start():
     menu = ["List all products in store", "Show total amount in store", "Make an order", "Quit"]
     my_menu = {
