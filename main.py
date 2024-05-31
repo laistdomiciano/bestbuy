44f1dca39bb32aa76dd7035c2a2d4c753b601b0e
Lais  Trevizan Domiciano <laistdomiciano@gamil.com>
2024-05-31 11:15:36 +0100

    Added new product types

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from products import Product
+from products import Product, NonStockedProduct, LimitedProduct
 from store import Store
 
 
-product_list = [
-    Product("MacBook Air M2", price=1450, quantity=100),
-    Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-    Product("Google Pixel 7", price=500, quantity=250),
-]
-
+# setup initial stock of inventory
+product_list = [ Product("MacBook Air M2", price=1450, quantity=100),
+                 Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 Product("Google Pixel 7", price=500, quantity=250),
+                 NonStockedProduct("Windows License", price=125),
+                 LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
+               ]
 best_buy = Store(product_list)
 
 
@@ -91,3 +92,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
--- products.py
+++ products.py
@@ -43,3 +43,34 @@ class Product:
             self.deactivate()
 
         return total_price
+
+class NonStockedProduct(Product):
+    def __init__(self, name, price):
+        super().__init__(name, price, quantity = 0)
+
+    def set_quantity(self, quantity):
+        raise Exception("Cannot set quantity for non-stocked product.")
+
+    def show(self):
+        return f"{self.name}, Price: {self.price}, Quantity: N/A"
+
+
+class LimitedProduct(Product):
+    def __init__(self, name, price, quantity, maximum):
+        super().__init__(name, price, quantity)
+        self.maximum = maximum
+
+    def buy(self, quantity):
+        if quantity > self.maximum:
+            raise Exception(f"Cannot purchase more than {self.maximum} of this product in a single order.")
+        return super().buy(quantity)
+
+    def show(self):
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}, Maximum per order: {self.maximum}"
+
+# test_non_stocked = NonStockedProduct('Windows License', 125)
+# print(test_non_stocked.show())
+# test_limited = LimitedProduct('Shipping', 10, quantity=250, maximum=1)
+# print(test_limited.show())
+
+
